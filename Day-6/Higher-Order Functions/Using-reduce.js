a5244646b3a5a26cc5cd1bfda77038d1f1cb95d4
bknguyenvanquy <quynguyenbk98@gmail.com>
2019-01-06 23:06:47 +0700

    update

--- Day-6/Higher-Order Functions/Using-reduce.js
+++ Day-6/Higher-Order Functions/Using-reduce.js
@@ -12,5 +12,12 @@ function reverse(array){
         return accumulator;
     },[]);
 }
-
 console.log(reverse([1,2,3,4,5,6]));
+
+
+function product(array){
+    return array.reduce(function(accumulator, currentValue){
+        return accumulator*currentValue;
+    },1);
+}
+console.log(product([1,2,3,4,5,6]));
