3244d821a7bbdd97d4bb026858638030ddc72197
bknguyenvanquy <quynguyenbk98@gmail.com>
2019-01-08 10:51:19 +0700

    update

--- Day-8/Excel-Column-Number.js
+++ Day-8/Excel-Column-Number.js
@@ -1,14 +1,4 @@
-// function caculateNumberCol(arr){
-//     return arr.reduce(function(accumulator, currentValue, currentIndex){
-//         accumulator.push(`${currentValue}->${currentIndex+1}`);
-//         return accumulator;
-//     },[]);
-// }
 
-// console.log(caculateNumberCol(['A','B','C','D','E','F']));
-// let str = "A"
-
-// console.log(str.charCodeAt(0));
 
 function indexChar(str,i){
     return str.charCodeAt(i) - 64;
