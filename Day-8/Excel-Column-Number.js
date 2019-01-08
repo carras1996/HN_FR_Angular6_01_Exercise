f56802430efcaf4c429df9d90f114ef121e9d7b9
bknguyenvanquy <quynguyenbk98@gmail.com>
2019-01-08 10:37:40 +0700

    update

--- Day-8/Excel-Column-Number.js
+++ Day-8/Excel-Column-Number.js
@@ -13,14 +13,17 @@
 function indexChar(str,i){
     return str.charCodeAt(i) - 64;
 }
-
-console.log(indexChar('A'));
+console.log(indexChar('B'));
 
 function indexCol(str){
+    debugger;
     let index = 0;
     let arr = str.split('');
     for(let i = 0; i<arr.length; i++){
-        index = 
+        index += indexChar(str,i)*Math.pow(26,arr.length-1-i);
     }
+    return index;
 }
+
+console.log(indexCol('BZA'));
 
