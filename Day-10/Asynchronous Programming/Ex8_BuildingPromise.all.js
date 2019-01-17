248e8b92dcdd1fda70c1bd308a80a1d95b1005d7
bknguyenvanquy <quynguyenbk98@gmail.com>
2019-01-17 09:59:49 +0700

    update

--- Day-10/Asynchronous Programming/Ex8_BuildingPromise.all.js
+++ Day-10/Asynchronous Programming/Ex8_BuildingPromise.all.js
@@ -2,21 +2,28 @@ function Promise_all(promises) {
     let result = [];
     return new Promise((resolve, reject) => {
         // Your code here.
-        async function execute() {
-            for (let element of promises) {
-                let data = await element;
+        // async function execute() {
+        //     for (let element of promises) {
+        //         let data = await element;
+        //         result.push(data);
+        //     };
+        //     resolve(result);
+        // }
+        // execute();
+        let count = promises.length;
+        promises.forEach((promise, index) => {
+            promise.then((data) => {
                 result.push(data);
-            };
-            resolve(result);
-        }
-        execute();
+                count--;
+                if (count == 0) {
+                    resolve(result);
+                }
+            })
+        })
     });
 }
 
 // Test code.
-Promise_all([]).then(array => {
-    console.log("This should be []:", array);
-});
 function soon(val) {
     return new Promise(resolve => {
         setTimeout(() => resolve(val), Math.random() * 500);
@@ -25,12 +32,12 @@ function soon(val) {
 Promise_all([soon(1), soon(2), soon(3)]).then(array => {
     console.log("This should be [1, 2, 3]:", array);
 });
-Promise_all([soon(1), Promise.reject("X"), soon(3)])
-    .then(array => {
-        console.log("We should not get here");
-    })
-    .catch(error => {
-        if (error != "X") {
-            console.log("Unexpected failure:", error);
-        }
-    });
+// Promise_all([soon(1), Promise.reject("X"), soon(3)])
+//     .then(array => {
+//         console.log("We should not get here");
+//     })
+//     .catch(error => {
+//         if (error != "X") {
+//             console.log("Unexpected failure:", error);
+//         }
+//     });
