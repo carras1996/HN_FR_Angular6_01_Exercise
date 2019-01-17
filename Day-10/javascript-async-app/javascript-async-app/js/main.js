05a37d4f4314ffb3c5ced7d69356194e7ccfed0f
bknguyenvanquy <quynguyenbk98@gmail.com>
2019-01-17 10:31:21 +0700

    update ex2 mini app

--- Day-10/javascript-async-app/javascript-async-app/js/main.js
+++ Day-10/javascript-async-app/javascript-async-app/js/main.js
@@ -28,7 +28,35 @@
     console.log(result);
   }
 })();*/
-(function() {
+// (function() {
+//   var app = angular.module("myApp", []);
+
+//   app.controller("demoCtrl", ["$scope", DemoController]);
+
+//   function DemoController($scope) {
+// Your Code Here
+// Callback
+//     $.get("https://jsonplaceholder.typicode.com/users", function(users) {
+//       users.forEach((user) => {
+//         $.get(`https://jsonplaceholder.typicode.com/posts?userId=${user.id}`, (posts) => {
+//           user.posts = posts;
+//           posts.forEach((postId) => {
+//             $.get(`https://jsonplaceholder.typicode.com/comments?postId=${postId.id}`, (comments) => {
+//               postId.comments = comments;
+//               $scope.$apply(() => {
+//                 $scope.users = users;
+//               })
+//             })
+//           });
+//         })
+//       })
+//     });
+//   }
+
+// Promise
+
+// })();
+(function () {
   var app = angular.module("myApp", []);
 
   app.controller("demoCtrl", ["$scope", DemoController]);
@@ -36,23 +64,39 @@
   function DemoController($scope) {
     // Your Code Here
     // Callback
-    $.get("https://jsonplaceholder.typicode.com/users", function(users) {
-      users.forEach((user) => {
-        $.get(`https://jsonplaceholder.typicode.com/posts?userId=${user.id}`, (posts) => {
-          user.posts = posts;
-          posts.forEach((postId) => {
-            $.get(`https://jsonplaceholder.typicode.com/comments?postId=${postId.id}`, (comments) => {
-              postId.comments = comments;
-              $scope.$apply(() => {
-                $scope.users = users;
-              })
-            })
-          });
-        })
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(users => {
+        // console.log(users.json());
+        return users.json();
       })
-    });
+      .then(users => {
+        // console.log(users);
+        users.forEach(user => {
+          fetch(`https://jsonplaceholder.typicode.com/posts?userId=${user.id}`)
+            .then(posts => {
+              return posts.json();
+            })
+            .then(posts => {
+              user.posts = posts;
+              // console.log(users);
+              posts.forEach(post => {
+                fetch(`https://jsonplaceholder.typicode.com/comments?postId=${post.id}`)
+                  .then(comments => {
+                    return comments.json();
+                  })
+                  .then(comments => {
+                    post.comments = comments;
+                    console.log(users);
+                  })
+              });
+            })
+        });
+
+      })
+      .catch(err => {
+      });
   }
 
   // Promise
-  
+
 })();
